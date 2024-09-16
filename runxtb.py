7965929317bc2ceabd6f91c3261040b4f8c131ef
Britton <bstan218@gmail.com>
2024-09-16 10:34:50 -0600

    added runxtb

---
+++ runxtb.py
@@ -0,0 +1,29 @@
+import subprocess
+import os
+
+
+
+#takes in a .xyz file, returns an optimized .mol
+def xtb_optimize(xyzfile, freezeatoms=None):
+    if freezeatoms:
+        with open("freeze.inp", 'w') as outf:
+            outf.write(f"$fix\n  atoms: {",".join(freezeatoms)}\n$end")
+    
+    os.environ['OMP_STACKSIZE'] = '1G'
+    runscript = ["xtb", f'{xyzfile}', "--opt", "[crude]"]
+    if freezeatoms:
+        runscript.append("--input")
+        runscript.append("freeze.inp")
+    subprocess.run(runscript)
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    outfdir = ""
